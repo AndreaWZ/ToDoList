35dfefbb042eb1d633d614a59e773b255b2fcb52
Andrea Wu <lixianhyun@gmail.com>
2020-05-25 23:40:17 -0700

    replacing 'p' by 'li' to display to-do list, rewrite delete-list button's code, add clean-up buttons and change body background-color

--- index.js
+++ index.js
@@ -1,63 +1,20 @@
-const items = [
-    {item:'homework', priority:1}, 
-    {item:"laundry", priority:2},
-    {item:'homework', priority:1}, 
-    {item:"laundry", priority:2},
-    {item:'homework', priority:1}, 
-    {item:"laundry", priority:2}
-];
-
-const content = $("#contents")[0];
-
-for(let i = 0; i < items.length; i++) {
-    const itemDom = document.createElement("div");
-    const textDom = document.createElement("p");
-    const priorityDom = document.createElement("span");
-
-    textDom.textContent = items[i].item;
-    priorityDom.textContent = items[i].priority;
-
-    content.appendChild(itemDom);
-    itemDom.appendChild(textDom);
-    itemDom.appendChild(priorityDom);
-
-    itemDom.classList.add("itemlist");
-}
-
-const textList = $("p");
-const priorityList = $("span");
-
-for(let i = 0; i < textList.length; i++) {
-    textList[i].style.textDecoration = "none";
-
-    if(i === textList.length -1) {
-        textList[i].style.borderBottom = "2px black solid";
-        priorityList[i].style.borderBottom = "2px black solid";
-    };
-
-    textList[i].addEventListener("click", function() {
-        if(textList[i].style.textDecoration === "line-through") {
-            textList[i].style.textDecoration = "none";
-        } else {
-            textList[i].style.textDecoration = "line-through";
-
-        }
-    });
+function deleteList() {
+    const allList = $("ul");
+    allList.remove();
 };
 
-function deleteList() {
-    items.splice(0, items.length);
-    content.remove();
+$("li").click(function() {
 
-    const contentsDiv = document.createElement("div");
-    contentsDiv.setAttribute("id", "contents");
+    $(this).toggleClass("completed");
+});
 
-    const wrapper = $("#wrapper")[0];
-    wrapper.appendChild(contentsDiv);
-}
+$("#cleanUp").click(function(){
+    let textList = $("li");
 
-// $(".submit").click(function(){
-//     const itemName = $(".text").text();
-//     const itemPriority = $(".select");
-// });
+    for(let i = 0; i < textList.length; i++) {
+        if(textList[i].className === "completed"){
+            textList[i].remove();
+        };
+    };
+})
 
